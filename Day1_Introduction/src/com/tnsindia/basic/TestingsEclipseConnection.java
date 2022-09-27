51d05c5943057340ee1fa986f99b4eeee6215b09
LENOVO <LENOVO@Rahul>
2022-09-27 23:48:20 +0530

    first commit

---
+++ Day1_Introduction/src/com/tnsindia/basic/TestingsEclipseConnection.java
@@ -0,0 +1,10 @@
+package com.tnsindia.basic;
+
+public class TestingsEclipseConnection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Wlcome to TNS India Foundation");
+	}
+
+}
