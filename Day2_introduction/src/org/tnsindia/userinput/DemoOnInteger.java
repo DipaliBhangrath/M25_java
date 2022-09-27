7ee479636c356d726061b07f259b40a49753bd2c
LENOVO <LENOVO@Rahul>
2022-09-28 00:32:09 +0530

    second commit

---
+++ Day2_introduction/src/org/tnsindia/userinput/DemoOnInteger.java
@@ -0,0 +1,17 @@
+package org.tnsindia.userinput;
+
+import java.util.Scanner;
+
+public class DemoOnInteger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		@SuppressWarnings("resource")
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the value of X : ");
+		int x = s.nextInt();
+		System.out.println("Result is : ");
+		System.out.print(x);
+	}
+
+}
